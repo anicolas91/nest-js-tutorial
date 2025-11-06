bff393cfe27df2b34b79bfb18498c64e7d2a5416
anicolas91 <andreanicolas91@gmail.com>
2025-11-06 14:10:11 -0700

    feat(auth.controller-and-service): added dummy fcns for login and signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 export class AuthController { 
     constructor(private authService: AuthService) { } /// I don't care how to instantiate, just do it for me
 
+    @Post('signup')
+    signup() {
+        return 'i am signed up';
+    }
+
+    @Post('signin')
+    signin() {
+        return 'i am signed in';
+    }
 }
